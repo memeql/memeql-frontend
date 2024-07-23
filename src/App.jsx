f96a59e41ad3f4744bfdf9f7f363607895d8389e
Konstantin <konstantin.kaminskiy@outlook.com>
2024-07-23 09:28:51 -0400

    lets not even mess with state in getting the url

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,29 @@
 import Demo from './components/Demo.jsx'
 import Memes from './components/Memes.jsx'
-import { useEffect, useState } from "react"
 import './App.css'
 
 function App() {
-  
-  const [baseBackendURL, setBaseBackendUrl] = useState('')
 
   const getBaseBackendURL = () => {
        switch (window.location.origin) {
         case "https://memeql.com":
           console.log("Running in the production environment")
-          setBaseBackendUrl("https://backend-memeql-prod.azurewebsites.net/")
-          break
+          return ("https://backend-memeql-prod.azurewebsites.net/")
         case "https://dev.memeql.com":
           console.log("Running in the development environment")
-          setBaseBackendUrl("https://backend-memeql-dev.azurewebsites.net/")
-          break
+          return ("https://backend-memeql-dev.azurewebsites.net/")
         default: 
           console.log("Running in the local environment")
-          setBaseBackendUrl("http://localhost:4000/")
-          break
+          return("http://localhost:4000/")
        }
   }
-
-  useEffect(() => {
-    getBaseBackendURL()
-  }, [])
   
   return (
     <>
       <div className='Demo'>
-        <Demo baseBackendURL = {baseBackendURL}/>
+        <Demo baseBackendURL = {getBaseBackendURL()}/>
       </div>
-      <Memes baseBackendURL = {baseBackendURL}/>
+      <Memes baseBackendURL = {getBaseBackendURL()}/>
     </>
   )
 }
